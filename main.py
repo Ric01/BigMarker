ad027e0643cb7fce961fd8ae071bb367be369a82
Ricardo Suarez <ricardo.suarez.atencio@gmail.com>
2020-07-27 14:56:02 -0400

    Update main.py
    Changed the call from list conferences to search conferences

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 """ =============================BIG MARKER API Connection Script======================="""
 """ This Module is designed to retrieve information from the Big Marker Webinar platform """
 """ So you can move it to your own CRM, """
+
 import bmconn as bm
 
-BM_API_KEY = "c15e450a5757c2e65ff8"
+BM_API_KEY = ""
 
 bm1 = bm.Bmconn(BM_API_KEY)
 
 print(bm1.api_key)
 
 data = bm1.get_conferences()
+listado = bm1.search_conferences()
 
-for conf in data:
-    print(conf['title'])
-    print(conf['id'])
+for conf in listado:
+    print("Title: ",conf['title'])
+    print("id: ",conf['id'])
+    print("copy_webinar_id:", conf['copy_webinar_id'])
     asist = bm1.get_atendees(conf['id'])
+    if asist is not None:
+        for attendee in asist:
+            if attendee['email'] is not None and attendee['first_name'] and attendee['last_name']:
+                print(attendee['email'] + ", " + attendee['first_name'] + ", "+ attendee['last_name']+", dur:"+ attendee['engaged_duration'])
+           
 
-asist = bm1.get_atendees("a7eef6bef481")
+    #asist2 = bm1.get_atendees(conf['copy_webinar_id'])
+
+#asist = bm1.get_atendees("bb47785703bc")
+
+
+#print(listado)
