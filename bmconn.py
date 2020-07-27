6019f097232d63ab40b13d8fb84818e571154d62
Ricardo Suarez <ricardo.suarez.atencio@gmail.com>
2020-07-27 14:54:58 -0400

    Update bmconn.py
    Modified the list conference call with the search conference, there are inconsistencies between each endpoints and the conference id.

--- bmconn.py
+++ bmconn.py
@@ -1,4 +1,6 @@
 """ BIG MARKER API CONNECTION MODULE """
+""" There are inconsistencies between the conference id from the search conferences and list conferences API """
+
 import requests
 from requests.exceptions import HTTPError
 
@@ -6,7 +8,7 @@ from requests.exceptions import HTTPError
 bm_show_conf_details_url = "https://www.bigmarker.com/api/v1/conferences/conference_id"
 bm_show_atendee_list_url = "https://www.bigmarker.com/api/v1/conferences/"
 bm_show_conf_list_url = "https://www.bigmarker.com/api/v1/conferences/"
-
+bm_search_conf = "https://www.bigmarker.com/api/v1/conferences/search/"
 
 
 class Bmconn:
@@ -33,11 +35,31 @@ class Bmconn:
             print(response_Json['conferences'])
             return response_Json['conferences']
 
+    def search_conferences(self):
+        """ Search a list of conferences from Big Marker"""
+        url = bm_search_conf       
+        header = {'Content-type': 'application/json', 'API-KEY':self.api_key}
+        try:
+            response = requests.post(url,headers = header)
+            print("Status code: ", response.status_code)
+            response_Json = response.json()
+            # If the response was successful, no Exception will be raised
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')  # Python 3.6
+        except Exception as err:
+            print(f'Other error occurred: {err}')  # Python 3.6
+        else:
+            print('Success!')
+            #print("Printing Post JSON data del método search:")
+            #print(response_Json['conferences'])
+            return response_Json['conferences']
+
     def get_conf_id(self,title):
         """ Returns a conference id from a given conference title """
 
     def get_atendees(self,conference_id):
-        """ Retrieves the list of atendees from a given Big Marker conference id"""
+        """ Retrieves the list of atendees from a given Big Marker conference id """
         url = bm_show_atendee_list_url + conference_id + "/attendees"
         print("Attendees url:",url)
 
@@ -53,8 +75,7 @@ class Bmconn:
         except Exception as err:
             print(f'Other error occurred: {err}')  # Python 3.6
         else:
-            print('Success!')
-            print("Printing Post JSON data")
-            print(response_Json)
-            return response_Json
+            print('Success getting attendees data!')
+            #print(response_Json['attendees'])
+            return response_Json['attendees']
     
